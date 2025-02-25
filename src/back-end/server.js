7cf78b3edbe03e29cf8e9b4bda9d5c9ecdf687f4
maclux <tim@geohei.lu>
2025-02-25 09:39:50 +0100

    feat: remove users endpoint

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -126,17 +126,6 @@ app.post("/login", (req, res) => {
 //     });
 // });
 
-// Unauthenticated API endpoint (exposes user data)
-app.get("/users", (req, res) => {
-  db.all("SELECT * FROM users", [], (err, rows) => {
-    if (err) {
-      res.status(500).send("Database error");
-      return;
-    }
-    res.json(rows);
-  });
-});
-
 // Serve frontend files
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "front-end", "index.html"));
